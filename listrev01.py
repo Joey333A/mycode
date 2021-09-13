c68d50f38d58e9b1c32562e613ea3dbdeb420890
Joey333A <joey333adams@gmail.com>
2021-09-13 17:20:13 +0000

    learning about lists

---
+++ listrev01.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Learning or Reviewing about Lists | by Alta3 Research"""
+
+def main():
+    myemptylist = []
+    myemptylist.extent('192.168.102.55')
+    print(myemptylist)
+
+if __name__ == "__main__":
+    main()
